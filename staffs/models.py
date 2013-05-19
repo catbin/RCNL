b550ee1ece8c165eaf3a326119d98d5277ebcb81
Xin <catbin@ovi.com>
2013-05-19 14:27:57 +1000

    all

--- staffs/models.py
+++ staffs/models.py
@@ -33,3 +33,11 @@ class Employement (models.Model):
 	
 	def __unicode__ (self):
 		return self.staff.name +"@"+self.shop.shop_name
+	
+class Day_hour (models.Model):
+	staff = models.ForeignKey('Staff')
+	day = models.DateField()
+	hours = models.DecimalField(max_digits=3,decimal_places=1)
+	
+	def __unicode__ (self):
+		return self.staff.name + '_worked_' + str(self.hours) + '_hours_on_' + self.day.strftime("%d-%m-%Y")
