211b5cbe1163ecac27605cc48a2b80ea2d0f5fe6
Xin <catbin@ovi.com>
2013-05-19 14:30:15 +1000

    Revert "all"
    This reverts commit b550ee1ece8c165eaf3a326119d98d5277ebcb81.

--- staffs/admin.py
+++ staffs/admin.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
-from staffs.models import Staff, Salary, Employement, Shop, Day_hour
+from staffs.models import Staff, Salary, Employement, Shop
 
 admin.site.register(Staff)
 admin.site.register(Salary)
 admin.site.register(Employement)
 admin.site.register(Shop)
-admin.site.register(Day_hour)
 
 
 
