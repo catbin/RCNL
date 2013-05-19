9ab85ccd0b138b4ee4a3ffca789142ac89a5a0e2
Xin <catbin@ovi.com>
2013-05-19 14:34:40 +1000

    e

--- staffs/admin.py
+++ staffs/admin.py
@@ -1,11 +1,11 @@
 
 from django.contrib import admin
-from staffs.models import Staff, Salary, Employement, Shop
+from staffs.models import Staff, Salary, Employement, Shop, Day_hour
 
 admin.site.register(Staff)
 admin.site.register(Salary)
 admin.site.register(Employement)
 admin.site.register(Shop)
-
+admin.site.register(Day_hour)
 
 
