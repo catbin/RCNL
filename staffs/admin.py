50d254781476faeba518c76a3f811ebe93164afb
Xin <catbin@ovi.com>
2013-05-19 14:20:55 +1000

    push

--- staffs/admin.py
+++ staffs/admin.py
@@ -2,6 +2,10 @@
 from django.contrib import admin
 from staffs.models import Staff, Salary, Employement, Shop
 
-admin.site.register(Staff, Salary, Employement, Shop)
+admin.site.register(Staff)
+admin.site.register(Salary)
+admin.site.register(Employement)
+admin.site.register(Shop)
+
 
 
