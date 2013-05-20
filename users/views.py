17d5c742e0b2b4e21d79dec07f76dc109794d919
Xin <catbin@ovi.com>
2013-05-20 23:45:58 +1000

    f

--- users/views.py
+++ users/views.py
@@ -5,10 +5,8 @@ from django.http import HttpResponseRedirect
 def login (request):
     if request.method == 'POST':
         form = UserFormToLogin(request.POST) 
-        if form.is_valid():
-            a = Authendicator 
-            result = a.login_me(form.cleaned_data['uname'], form.cleaned_data['password'], request)()
-            if result:
+        if form.is_valid(): 
+            if Authendicator.login_me(form.cleaned_data['uname'], form.cleaned_data['password'], request) == Authendicator.AUTHENDICATED:
                 return HttpResponseRedirect('/thanks/') # Redirect after POST
     else:
         form = UserFormToLogin()
